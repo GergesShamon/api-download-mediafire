5cac4106db73d23b1ed55b4ed3f814792a01839a
Gerges Shamon <76453737+GergesShamonDev@users.noreply.github.com>
2021-02-12 23:04:18 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,9 @@ $support_domain = 'www.mediafire.com';
 
 if(isset($_GET['url']) ) {
  $url = $_GET['url'];
+ 
+
+preg_match('@^(?:http.?://)?([^/]+)@i', $url, $matches);
   $host = $matches[1];
 
   if($host != $support_domain) {
@@ -20,7 +23,6 @@ if(isset($_GET['url']) ) {
 
   }else {
 $_succeeded = true; 
-preg_match('@^(?:http.?://)?([^/]+)@i', $url, $matches);
 $result = file_get_contents($url, false, stream_context_create(['socket' => ['bindto' => '0:0']])); // force IPv4
 
 preg_match('/aria-label="Download file"\n.+href="(.*)"/', $result, $matches);
@@ -29,8 +31,7 @@ $result = urldecode($matches[1]);
 
 }
 
-}
-else {
+}else {
       $_msg = 'Please input a valid mediafire url.';
 
       $_succeeded = false;
