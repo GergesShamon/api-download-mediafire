9e0559cf33cc357103787c2921225b4e7be48ac1
Gerges Shamon <gerges.shamoon.dev@gmail.com>
2022-01-20 21:28:07 +0200

    update main.api.php

--- main.api.php
+++ main.api.php
@@ -1,48 +1,27 @@
 <?php
-
-$_msg;
-$_succeeded;
-$result;
-
-
+class ApiMediafire{
+public $_msg;
+public $_succeeded;
+public $result;
 $support_domain = 'www.mediafire.com';
-
-
-if(isset($_GET['url']) ) {
- $url = $_GET['url'];
- 
-
-preg_match('@^(?:http.?://)?([^/]+)@i', $url, $matches);
+function get_url_download($url){
+  
+  preg_match('@^(?:http.?://)?([^/]+)@i', $url, $matches);
   $host = $matches[1];
-
   if($host != $support_domain) {
 
-    $_msg = 'Please input a valid mediafire url.';
-
-      $_succeeded = 'false';
-
+    die('Please input a valid mediafire url.');
   }else {
-$_succeeded = 'true';
+
 $result = file_get_contents($url, false, stream_context_create(['socket' => ['bindto' => '0:0']])); // force IPv4
 
 preg_match('/aria-label="Download file"\n.+href="(.*)"/', $result, $matches);
 
-$result = urldecode($matches[1]);
 
+return urldecode($matches[1]);
+}
+}
 }
 
-}else {
-      $_msg = 'Please input a valid mediafire url.';
-
-      $_succeeded = 'false';
-
-}
-
-
-
-
-
-
-echo '{"succeeded":"'.$_succeeded.'","message":"'.$_msg.'","url":"'.$result.'"}';
 
 ?>
